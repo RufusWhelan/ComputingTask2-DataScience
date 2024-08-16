746aa22923cdaaf95f9cdaa2ca64dc605455d53a
RufusWhelan <rufus.whelan@education.nsw.gov.au>
2024-08-17 09:57:19 +1000

    17/08/24
    The First thing I did today was fix how games containing the word 'Wave' were filtered out. using 'query' rather than the ~ operator didn't cause an error, so I used it remove rows that had a couple different words in them like 'DLC' or 'Edition'.
    
    I noticed a better way to remove columns I didn't need, that also allowed me to reorder the columns I was using, so I implemented that.
    
    I then sorted the games in to different categories based on their genre (I didn't include all genres, because some were things like 'golf' or 'Compilation and were likely all ready included in other genres'). using the '.contains' function which worked great. Next time I will start calculating the average scores from critics and users for each genre, so they can be compared.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import pandas as pd
+
+nintendoGames_df = pd.read_csv('Data/NintendoGames.csv') #Loads Dataframe
+
+nintendoGames_df.dropna(inplace=True)
+nintendoGames_df = nintendoGames_df[['title','meta_score','user_score','platform','genres']]
+nintendoGames_df.drop_duplicates(inplace=True)
+nintendoGames_df = nintendoGames_df[nintendoGames_df.platform != "iOS"]
+nintendoGames_df = nintendoGames_df.query(f'not title.str.contains("Wave")', engine='python')
+nintendoGames_df = nintendoGames_df.query(f'not title.str.contains("DLC")', engine='python')
+nintendoGames_df = nintendoGames_df.query(f'not title.str.contains("Edition")', engine='python')
+print(nintendoGames_df)
